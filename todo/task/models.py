ee129240d6c6829fe578ee12a66476b7681988a8
Ben McLean <bmcle071@uottawa.ca>
2022-03-15 17:02:26 -0400

    refactoring task module.
    I created a set of helper CRUD methods to manipulate tasks.

--- todo/task/models.py
+++ todo/task/models.py
@@ -1,9 +1,13 @@
 """Models relating to task"""
 
-from typing import TYPE_CHECKING
+from datetime import datetime
+from typing import TYPE_CHECKING, List, Union
+
+from flask_login import current_user
 
 
 from ..database import (
+    db,
     Boolean,
     Column,
     DateTime,
@@ -32,8 +36,57 @@ class Task(Model):
 
     completed = Column(Boolean, nullable=False, default=False)
 
-    user_id = Column(Integer, ForeignKey("user.user_id"), nullable=False, index=True)
+    user_id = Column(
+        Integer, ForeignKey("user.user_id"), nullable=False, index=True
+    )
     user: "User" = relationship("User", back_populates="tasks", lazy=False)  # type: ignore
 
     def __repr__(self) -> str:
         return f"Task: id={self.task_id}, title={self.title}"
+
+    @staticmethod
+    def complete(task_id: int) -> "Task":
+        """Complete task with given id in the database"""
+        task = Task.query.filter_by(task_id=task_id).first()
+        if task is None:
+            raise ValueError("No such task in database with task_id={task_id}")
+
+        task.completed = True
+        task.completed_at = datetime.utcnow()
+        db.session.commit()
+
+        return task
+
+    @staticmethod
+    def delete(task_id: int) -> "Task":
+        """Delete task with given id in the database"""
+        task = Task.query.filter_by(task_id=task_id).first()
+        if task is None:
+            raise ValueError("No such task in database with task_id={task_id}")
+
+        db.session.delete(task)
+        db.session.commit()
+
+        return task
+
+    @staticmethod
+    def create(
+        title: str, description: str, due: Union[datetime, None] = None
+    ) -> "Task":
+        """Task factory"""
+        new_task = Task(
+            title=title,
+            description=description,
+            created=datetime.utcnow(),
+            user_id=current_user.user_id,
+            due=due,
+        )
+        db.session.add(new_task)
+        db.session.commit()
+
+        return new_task
+
+    @staticmethod
+    def get_all() -> List["Task"]:
+        """Helper to return all tasks"""
+        return Task.query.filter_by(user_id=current_user.user_id).all()
--- todo/task/views.py
+++ todo/task/views.py
@@ -2,71 +2,97 @@
 
 from datetime import datetime
 from os import abort
+from typing import Union
 
-from flask import Blueprint, flash, redirect, render_template, url_for
+from flask import (
+    Blueprint,
+    Response,
+    flash,
+    redirect,
+    render_template,
+    url_for,
+)
 from flask_login import current_user
 
 from todo.task.forms import NewTaskForm
 
 from .models import Task
-from ..database import db
 
 task_blueprint = Blueprint("task", __name__, template_folder="templates")
 
 
-@task_blueprint.route("/task", methods=["GET", "POST"])
-def view_task_list():
-    """View all tasks"""
-
+def ensure_login() -> Union[None, Response]:
+    """
+    Helper to ensure the user is logged in
+    otherwise send them to the login screen
+    """
     if not current_user.is_authenticated:
         flash("You must be logged in to see this page.", "danger")
         return redirect(url_for("user.view_sign_in"))
 
-    # we don't paginate because users shouldn't have THAT
-    # many tasks
 
-    form = NewTaskForm()
-    if form.validate_on_submit():
-        new_task = Task(
-            title=form.title.data,
-            description=form.description.data,
-            created=datetime.utcnow(),
-            user_id=current_user.user_id,
-        )
-        db.session.add(new_task)
-        db.session.commit()
+def get_task_view(form: NewTaskForm = NewTaskForm()) -> str:
+    """Helper to return common view to PUT and GET methods"""
+    tasks = Task.get_all()
+    # alternatively we could query for complete and incomplete respectively.
+    # but I believe doing one query and then a filter will be quicker
 
-    incomplete_tasks = Task.query.filter_by(
-        user_id=current_user.user_id, completed=False
-    ).all()
+    incomplete_tasks = [t for t in tasks if not t.completed]
+    completed_tasks = [t for t in tasks if t.completed]
+
     return render_template(
-        "task_list.html", incomplete_tasks=incomplete_tasks, form=form
+        "task_list.html",
+        incomplete_tasks=incomplete_tasks,
+        completed_tasks=completed_tasks,
+        form=form,
     )
+
+
+@task_blueprint.route("/task", methods=["GET"])
+def view_task_list():
+    """View all tasks, and create tasks as required"""
+    send_to_login = ensure_login()
+    if send_to_login:
+        return send_to_login
+    else:
+        return get_task_view()
+
+
+@task_blueprint.route("/task", methods=["POST"])
+def create_task():
+    """Creates a task based on form input"""
+    send_to_login = ensure_login()
+    if send_to_login:
+        return send_to_login
+    else:
+        form = NewTaskForm()
+        if form.validate_on_submit():
+            Task.create(form.title.data, form.description.data)
+
+        return get_task_view(form)
 
 
 @task_blueprint.route("/task/complete/<int:task_id>", methods=["GET"])
 def complete_task(task_id: int):
     """Modify task_id to have completed attribute set to True"""
-    task = Task.query.filter_by(task_id=task_id).first()
-    if task is None:
-        abort("Task does not exist!")
+    send_to_login = ensure_login()
+    if send_to_login:
+        return send_to_login
+    else:
+        task = Task.complete(task_id)
 
-    task.completed = True
-    db.session.commit()
-
-    flash(f'Good job! You completed "{task.title}"', "success")
-    return redirect(url_for("task.view_task_list"))
+        flash(f'Good job! You completed "{task.title}"', "success")
+        return redirect(url_for("task.view_task_list"))
 
 
-@task_blueprint.route("/task/complete/<int:task_id>", methods=["GET"])
+@task_blueprint.route("/task/delete/<int:task_id>", methods=["GET"])
 def delete_task(task_id: int):
     """Remove task with associated ID from the database."""
-    task = Task.query.filter_by(task_id=task_id).first()
-    if task is None:
-        abort("Task does not exist!")
+    send_to_login = ensure_login()
+    if send_to_login:
+        return send_to_login
+    else:
+        old_task = Task.delete(task_id)
 
-    db.session.delete(task)
-    db.session.commit()
-
-    flash(f'You deleted "{task.title}"', "info")
-    return redirect(url_for("task.view_task_list"))
+        flash(f'You deleted "{old_task.title}"', "info")
+        return redirect(url_for("task.view_task_list"))
